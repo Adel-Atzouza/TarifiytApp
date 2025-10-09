15cdf995b2197a85637bc2ec0f6141794a2c03e6
Adel-Atzouza <adelatzouza@gmail.com>
2025-10-09 16:20:27 +0200

    remove mention of test in ci

--- backend/api/api/src/routers/lessons.py
+++ backend/api/api/src/routers/lessons.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter
 from dependency_injector.wiring import inject
 
 from api.api.src.containers.interactors import GetAllLessonsInteractorDep
+from api.domain.src.models.Lesson import Lesson
 
 router = APIRouter()
 
 
 @router.get("/lessons")
 @inject
-async def get_all_lessons(interactor: GetAllLessonsInteractorDep):
+async def get_all_lessons(interactor: GetAllLessonsInteractorDep) -> list[Lesson]:
     return interactor()
